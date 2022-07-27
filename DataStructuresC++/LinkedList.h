c90d2c972b4bc2bdee2382d35a5c0da01953a4d1
Ishfaq Wardag <ishfaqwardag@hotmail.co.uk>
2022-07-27 16:25:14 +0100

    Added iterator support for linked list and optimised code

--- DataStructuresC++/LinkedList.h
+++ DataStructuresC++/LinkedList.h
@@ -4,8 +4,129 @@
 #include <vector>
 #include <stdexcept>
 
+template<typename LinkedList>
+class ListIterator {
+public:
+    using ListType = typename LinkedList::Node;
+    using ValueType = typename LinkedList::ValueType;
+
+public:
+    ListIterator() : mPtr(nullptr) {}
+
+    ListIterator(ListType* ptr) : mPtr(ptr) {}
+
+    ListIterator(const ListIterator& other) : mPtr(other.mPtr) {}
+
+    ListIterator& operator=(const ListIterator& other) {
+        if (this != &other)
+            mPtr = other.mPtr;
+        return *this;
+    }
+
+    ListIterator(ListIterator&& other) noexcept {
+        mPtr = other.mPtr;
+        other.mPtr = nullptr;
+    }
+
+    ListIterator& operator=(ListIterator&& other) noexcept {
+        if (this != &other) {
+            mPtr = other.mPtr;
+            other.mPtr = nullptr;
+        }
+        return *this;
+    }
+
+    ListIterator& operator++() {
+#ifdef DEBUG
+        if (mPtr) {
+#endif
+        mPtr = mPtr->next;
+        return *this;
+#ifdef DEBUG
+        }
+        throw std::out_of_range("Cannot increment list iterator past end of list");
+#endif
+    }
+
+    ListIterator operator++(int) {
+#ifdef DEBUG
+        if (mPtr) {
+#endif
+        ListIterator temp(*this);
+        ++*this;
+        return temp;
+#ifdef DEBUG
+        }
+        throw std::out_of_range("Cannot increment list iterator past end of list");
+#endif
+    }
+
+    ListIterator operator+(const size_t& amount) {
+        ListIterator result(*this);
+#ifdef DEBUG
+        size_t moved = 0;
+        while (mPtr && moved < amount) {
+            result.mPtr = result.mPtr->next;
+            ++moved;
+        }
+        if (moved == amount)
+            return result;
+        throw std::out_of_range("Cannot increment list iterator past end of list");
+#endif
+#ifdef RELEASE
+        for (size_t i = 0; i < amount; ++i)
+            result.mPtr = result.mPtr->next;
+        return result;
+#endif
+    }
+
+    ListIterator& operator+=(const size_t& amount) {
+#ifdef DEBUG
+        size_t moved = 0;
+        while (mPtr && moved < amount) {
+            mPtr = mPtr->next;
+            ++moved;
+        }
+        if (moved == amount)
+            return *this;
+        throw std::out_of_range("Cannot increment list iterator past end of list");
+#endif
+#ifdef RELEASE
+        for (size_t i = 0; i < amount; ++i)
+            mPtr = mPtr->next;
+        return *this;
+#endif
+    }
+
+    bool operator==(const ListIterator& other) const noexcept {
+        return mPtr == other.mPtr;
+    }
+
+    bool operator!=(const ListIterator& other) const noexcept {
+        return !(mPtr == other.mPtr);
+    }
+
+    ListIterator& advance() {
+        mPtr = mPtr->next;
+        return *this;
+    }
+
+    ValueType& operator*() {
+        return mPtr->data;
+    }
+
+private:
+    ListType* mPtr;
+};
+
 template<typename T>
 class LinkedList {
+public:
+    using ValueType = T;
+    using Iterator = ListIterator<LinkedList>;
+
+    friend class ListIterator<LinkedList>;
+
 public:
     LinkedList() : head(nullptr), tail(nullptr), mLength(0) {}
 
@@ -98,36 +219,40 @@ public:
     }
 
     void insert(const T& data, const size_t& index) {
+#ifdef DEBUG
         if (mLength && index <= mLength) {
-            Node* new_node = new Node(data);
-            ++mLength;
-            if (index == 0) {
-                new_node->next = head;
-                head = new_node;
+#endif
+        Node* new_node = new Node(data);
+        ++mLength;
+        if (index == 0) {
+            new_node->next = head;
+            head = new_node;
+            return;
+        }
+        if (index == mLength) {
+            tail->next = new_node;
+            tail = new_node;
+            return;
+        }
+        size_t _index = 1;
+        Node* cur_node = head;
+        Node* last_node;
+        while (true) {
+            last_node = cur_node;
+            cur_node = cur_node->next;
+            if (_index == index) {
+                last_node->next = new_node;
+                new_node->next = cur_node;
                 return;
             }
-            if (index == mLength) {
-                tail->next = new_node;
-                tail = new_node;
-                return;
-            }
-            size_t _index = 1;
-            Node* cur_node = head;
-            Node* last_node = nullptr;
-            while (1) {
-                last_node = cur_node;
-                cur_node = cur_node->next;
-                if (_index == index) {
-                    last_node->next = new_node;
-                    new_node->next = cur_node;
-                    return;
-                }
-                ++_index;
-            }
+            ++_index;
+        }
+#ifdef DEBUG
         }
         if (index > mLength)
             throw std::invalid_argument("Invalid index, out of range");
         throw std::invalid_argument("Linked list is empty and uninitialised, use append instead");
+#endif
     }
 
     void push_front(const T& data) {
@@ -135,41 +260,53 @@ public:
     }
 
     std::vector<T> contents() const {
+#ifdef DEBUG
         if (mLength) {
-            std::vector<T> elems(mLength);
-            Node* cur_node = head;
-            for (int i = 0; i < mLength; ++i) {
-                elems[i] = cur_node->data;
-                cur_node = cur_node->next;
-            }
-            return elems;
+#endif
+        std::vector<T> elems(mLength);
+        Node* cur_node = head;
+        for (int i = 0; i < mLength; ++i) {
+            elems[i] = cur_node->data;
+            cur_node = cur_node->next;
+        }
+        return elems;
+#ifdef DEBUG
         }
         throw std::runtime_error("Error: Linked list is empty");
+#endif
     }
 
     int find(const T& data) const {
+#ifdef DEBUG
         if (mLength) {
-            int index = 0;
-            Node* cur_node = head;
-            while (cur_node) {
-                if (cur_node->data == data)
-                    return index;
-                cur_node = cur_node->next;
-                ++index;
-            }
-            return -1;
+#endif
+        int index = 0;
+        Node* cur_node = head;
+        while (cur_node) {
+            if (cur_node->data == data)
+                return index;
+            cur_node = cur_node->next;
+            ++index;
+        }
+        return -1;
+#ifdef DEBUG
         }
         throw std::runtime_error("Error: Linked list is empty, there is no content to search");
+#endif
     }
 
     void display() const {
+#ifdef DEBUG
         if (mLength) {
-            std::vector<T> vals = contents();
-            for (const T& i: vals)
-                std::cout << i << "\t";
-            std::cout << "\n";
+#endif
+        std::vector<T> vals = contents();
+        for (const T& i: vals)
+            std::cout << i << "\t";
+        std::cout << "\n";
+#ifdef DEBUG
         }
         else throw std::runtime_error("Error: Linked list is empty");
+#endif
     }
 
     size_t length() const noexcept {
@@ -180,7 +317,7 @@ public:
         return mLength == 0;
     }
 
-    operator bool() const noexcept {
+    explicit operator bool() const noexcept {
         return (bool) mLength;
     }
 
@@ -199,118 +336,150 @@ public:
     }
 
     void erase(const size_t& index) {
+#ifdef DEBUG
         if (mLength && index < mLength) {
-            if (index == 0) {
-                Node* head_cpy = head;
-                head = head->next;
-                delete head_cpy;
+#endif
+        if (index == 0) {
+            Node* head_cpy = head;
+            head = head->next;
+            delete head_cpy;
+            --mLength;
+            return;
+        }
+        size_t cur_index = 1;
+        Node* cur_node = head;
+        while (true) {
+            Node* last_node = cur_node;
+            cur_node = cur_node->next;
+            if (cur_index == index) {
+                last_node->next = cur_node->next;
+                if (last_node->next == nullptr) {
+                    tail = last_node;
+                }
+                delete cur_node;
                 --mLength;
                 return;
             }
-            size_t cur_index = 1;
-            Node* cur_node = head;
-            while (1) {
-                Node* last_node = cur_node;
-                cur_node = cur_node->next;
-                if (cur_index == index) {
-                    last_node->next = cur_node->next;
-                    if (last_node->next == nullptr) {
-                        tail = last_node;
-                    }
-                    delete cur_node;
-                    --mLength;
-                    return;
-                }
-                ++cur_index;
-            }
+            ++cur_index;
+        }
+#ifdef DEBUG
         }
         if (index >= mLength)
             throw std::invalid_argument("Invalid index, out of range");
         throw std::runtime_error("Error: Linked list is empty, there is nothing to erase");
+#endif
     }
 
     void clear() {
+#ifdef DEBUG
         if (mLength) {
-            Node* cur_node = head;
-            while (cur_node) {
-                cur_node = cur_node->next;
-                delete head;
-                head = cur_node;
-            }
-            head = nullptr;
-            tail = head;
-            mLength = 0;
+#endif
+        Node* cur_node = head;
+        while (cur_node) {
+            cur_node = cur_node->next;
+            delete head;
+            head = cur_node;
+        }
+        head = nullptr;
+        tail = head;
+        mLength = 0;
+#ifdef DEBUG
         }
         else throw std::runtime_error("Error: linked list is empty and so cannot be cleared.");
+#endif
     }
 
     T get(const size_t& index) const {
+#ifdef DEBUG
         if (index < mLength) {
-            if (index == 0)
-                return head->data;
-            size_t cur_index = 1;
-            Node* cur_node = head;
-            while (1) {
-                Node* last_node = cur_node;
-                cur_node = cur_node->next;
-                if (cur_index == index)
-                    return cur_node->data;
-                ++cur_index;
-            }
+#endif
+        if (index == 0)
+            return head->data;
+        size_t cur_index = 1;
+        Node* cur_node = head;
+        while (1) {
+            Node* last_node = cur_node;
+            cur_node = cur_node->next;
+            if (cur_index == index)
+                return cur_node->data;
+            ++cur_index;
+        }
+#ifdef DEBUG
         }
         throw std::invalid_argument("Invalid index, out of range");
+#endif
     }
 
     T front() const {
+#ifdef DEBUG
         if (mLength)
-            return head->data;
+#endif
+        return head->data;
         throw std::runtime_error("List is empty, there is nothing at front");
     }
 
     T back() const {
+#ifdef DEBUG
         if (mLength)
-            return tail->data;
+#endif
+        return tail->data;
         throw std::runtime_error("List is empty, there is nothing at back");
     }
 
     void pop_front() {
+#ifdef DEBUG
         if (mLength)
-            erase(0);
+#endif
+        erase(0);
+#ifdef DEBUG
         else throw std::runtime_error("List is empty, there is nothing to pop front");
+#endif
     }
 
     void pop_back() {
+#ifdef DEBUG
         if (mLength)
-            erase(mLength - 1);
+#endif
+        erase(mLength - 1);
+#ifdef DEBUG
         else throw std::runtime_error("List is empty, there is nothing to pop back");
+#endif
     }
 
     void reverse_order() {
+#ifdef DEBUG
         if (mLength) {
-            Node* cur_node = head;
-            tail = head;
-            Node* last = nullptr;
-            Node* next = nullptr;
-            while (cur_node) {
-                next = cur_node->next;
-                cur_node->next = last;
-                last = cur_node;
-                cur_node = next;
-            }
-            head = last;
+#endif
+        Node* cur_node = head;
+        tail = head;
+        Node* last = nullptr;
+        Node* next;
+        while (cur_node) {
+            next = cur_node->next;
+            cur_node->next = last;
+            last = cur_node;
+            cur_node = next;
+        }
+        head = last;
+#ifdef DEBUG
         }
         else throw std::runtime_error("Error: linked list is empty and so cannot be reversed");
+#endif
     }
 
     T operator[](const size_t& index) const {
+#ifdef DEBUG
         if (index < mLength)
-            return get(index);
+#endif
+        return get(index);
         throw std::invalid_argument("Invalid index, out of range");
     }
 
     T& operator[](const size_t index) {
+#ifdef DEBUG
         if (index < mLength)
-            return ref_get(index);
+#endif
+        return ref_get(index);
         throw std::invalid_argument("Invalid index, out of range");
     }
 
@@ -326,6 +495,14 @@ public:
             return *res;
         }
         return *this;
+    }
+
+    Iterator begin() const {
+        return Iterator(head);
+    }
+
+    Iterator end() const {
+        return Iterator(nullptr);
     }
 
     virtual ~LinkedList() {
@@ -346,19 +523,23 @@ private:
     size_t mLength;
 
     T& ref_get(const size_t& index) {
+#ifdef DEBUG
         if (index < mLength) {
-            if (index == 0)
-                return head->data;
-            size_t cur_index = 1;
-            Node* cur_node = head;
-            while (1) {
-                Node* last_node = cur_node;
-                cur_node = cur_node->next;
-                if (cur_index == index)
-                    return cur_node->data;
-                ++cur_index;
-            }
+#endif
+        if (index == 0)
+            return head->data;
+        size_t cur_index = 1;
+        Node* cur_node = head;
+        while (true) {
+            Node* last_node = cur_node;
+            cur_node = cur_node->next;
+            if (cur_index == index)
+                return cur_node->data;
+            ++cur_index;
+        }
+#ifdef DEBUG
         }
         throw std::invalid_argument("Invalid index, out of range");
+#endif
     }
 };
--- DataStructuresC++/main.cpp
+++ DataStructuresC++/main.cpp
@@ -15,39 +15,45 @@
 
 template<typename T>
 void printvec(std::vector<T> data) {
-	for (T& i : data) {
-		std::cout << i << " ";
-	}
-	std::cout << std::endl;
+    for (T& i: data) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main() {
-	try {
-		LinkedList<float> list(2.31f);
-		list.append(3.13f);
-		list.append(5.87f);
-		list.append(15.55f);
-		list.append(45.90f);
-		list.display();
-		std::cout << list.length() << std::endl;
-		list.erase(2);
-		list.display();
-		std::cout << list[0] <<"\n\n";
+    try {
+        LinkedList<float> list(2.31f);
+        list.append(3.13f);
+        list.append(5.87f);
+        list.append(15.55f);
+        list.append(45.90f);
+        list.display();
+        std::cout << list.length() << std::endl;
+        list.erase(2);
+        list.display();
+        std::cout << list[0] << "\n\n";
 
-		list.reverse_order();
-		list.display();
-		std::cout << "\n";
+        auto it = list.begin();
+        std::cout << *it++ << "\n";
+        std::cout << *it++ << "\n";
+        std::cout << *it << "\n";
+        std::cout << "\n\n";
 
-		LinkedList<float> list2;
-		list2.append(1.90f);
-		list2.append(2.90f);
-		list2.append(3.90f);
-		list2.display();
-		std::cout << "\n";
+        list.reverse_order();
+        list.display();
+        std::cout << "\n";
 
-		LinkedList<float> list3 = list + list2;
-		list3.display();
-		std::cout << "\n\n";
+        LinkedList<float> list2;
+        list2.append(1.90f);
+        list2.append(2.90f);
+        list2.append(3.90f);
+        list2.display();
+        std::cout << "\n";
+
+        LinkedList<float> list3 = list + list2;
+        list3.display();
+        std::cout << "\n\n";
 
         LinkedList<float> list4;
         list4 = list3;
@@ -55,149 +61,152 @@ int main() {
         std::cout << "\n\n";
 
         Queue<std::string> queue;
-		queue.enqueue("Hello ");
-		queue.enqueue("my ");
-		queue.enqueue("name ");
-		queue.enqueue("is ");
-		std::cout << "Peeking Queue: " << queue.peek() << "\n";
-		queue.enqueue("Bart");
-		queue.dequeue();
-		std::cout << "length = " << queue.length() << "\n";
-		std::cout << "Peeking Queue: " << queue.peek() << "\n";
-		queue.display();
-		std::cout << queue.contains("is ") << "\n";
-		std::cout << queue.length() << "\n\n";
+        queue.enqueue("Hello ");
+        queue.enqueue("my ");
+        queue.enqueue("name ");
+        queue.enqueue("is ");
+        std::cout << "Peeking Queue: " << queue.peek() << "\n";
+        queue.enqueue("Bart");
+        queue.dequeue();
+        std::cout << "length = " << queue.length() << "\n";
+        std::cout << "Peeking Queue: " << queue.peek() << "\n";
+        queue.display();
+        std::cout << queue.contains("is ") << "\n";
+        std::cout << queue.length() << "\n\n";
 
-		Stack<char> stack;
-		stack.push('a');
-		stack.push('n');
-		stack.push('d');
-		stack.push('k');
-		stack.display();
-		stack.pop();
-		std::cout << stack.peek() << "\n";
-		std::cout << stack.length() << "\n\n";
+        Stack<char> stack;
+        stack.push('a');
+        stack.push('n');
+        stack.push('d');
+        stack.push('k');
+        stack.display();
+        stack.pop();
+        std::cout << stack.peek() << "\n";
+        std::cout << stack.length() << "\n\n";
 
-		BT::BinaryTree<int> Btree = BT::BinaryTree<int>(5);
-		Btree.new_left(10);
-		Btree.new_right(15);
-		Btree.advance_left();
-		Btree.new_left(20);
-		Btree.new_right(25);
-		Btree.goto_root();
-		Btree.advance_right();
-		Btree.new_left(30);
-		Btree.new_right(35);
-		std::vector<int> data = Btree.contents_InOrder();
-		printvec(data);
-		data = Btree.contents_PostOrder();
-		printvec(data);
-		data = Btree.contents_PreOrder();
-		printvec(data);
-		std::cout << Btree.max_height() << std::endl;
+        BT::BinaryTree<int> Btree = BT::BinaryTree<int>(5);
+        Btree.new_left(10);
+        Btree.new_right(15);
+        Btree.advance_left();
+        Btree.new_left(20);
+        Btree.new_right(25);
+        Btree.goto_root();
+        Btree.advance_right();
+        Btree.new_left(30);
+        Btree.new_right(35);
+        std::vector<int> data = Btree.contents_InOrder();
+        printvec(data);
+        data = Btree.contents_PostOrder();
+        printvec(data);
+        data = Btree.contents_PreOrder();
+        printvec(data);
+        std::cout << Btree.max_height() << std::endl;
 
-		Btree.clear();
-		Btree.change_data(10);
-		std::cout << "\nTree height after clearing: " << Btree.max_height();
-		std::cout << "\n\n";
+        Btree.clear();
+        Btree.change_data(10);
+        std::cout << "\nTree height after clearing: " << Btree.max_height();
+        std::cout << "\n\n";
 
-		GP::Graph<int> graph(5);
-		graph.add_node(10);
-		graph.add_node(15);
-		graph.add_node(20);
-		graph.add_node(30);
-		graph.add_edge('A', 'B');
-		graph.add_edge('B', 'C');
-		graph.add_edge('A', 'D');
-		graph.add_edge('A', 'C');
-		graph.add_edge('C', 'D');
-		graph.add_edge('B', 'D');
-		graph.add_edge('A', 'E');
-		graph.add_edge('E', 'B');
-		graph.print();
-		std::cout << "\n";
-		graph.remove('C');
-		graph.print();
-		std::cout << "\n\n";
+        GP::Graph<int> graph(5);
+        graph.add_node(10);
+        graph.add_node(15);
+        graph.add_node(20);
+        graph.add_node(30);
+        graph.add_edge('A', 'B');
+        graph.add_edge('B', 'C');
+        graph.add_edge('A', 'D');
+        graph.add_edge('A', 'C');
+        graph.add_edge('C', 'D');
+        graph.add_edge('B', 'D');
+        graph.add_edge('A', 'E');
+        graph.add_edge('E', 'B');
+        graph.print();
+        std::cout << "\n";
+        std::cout << graph.find_edge('C', 'A');
+        std::cout << "\n";
+        graph.remove('C');
+        graph.print();
+        std::cout << "\n\n";
 
-		Queue<int> PQueue(3, 1);
-		PQueue.enqueue(15);
-		PQueue.enqueue(12);
-		PQueue.enqueue(20);
-		PQueue.enqueue(7);
-		PQueue.display();
-		std::cout << "\n\n";
+        Queue<int> PQueue(3, 1);
+        PQueue.enqueue(15);
+        PQueue.enqueue(12);
+        PQueue.enqueue(20);
+        PQueue.enqueue(7);
+        PQueue.display();
+        std::cout << "\n\n";
 
-		Map::Map<int> map;
-		map.add("A", 15);
-		map.add("B", 25);
-		map.add("C", 6);
-		map.add("D", 53);
-		map.add("E", 152);
-		map.print();
-		std::cout << "\n\n";
+        Map::Map<int, const char*> map;
+        map.add("A", 15);
+        map.add("B", 25);
+        map.add("C", 6);
+        map.add("D", 53);
+        map.add("E", 152);
+        map["F"] = 196;
+        map.print();
+        std::cout << "\n\n";
 
-		BST::BinarySearchTree<double> bst(2.5);
-		bst.add(1.9);
-		bst.add(5.6);
-		bst.add(8.3);
-		bst.add(1.2);
-		bst.add(12.9);
-		bst.remove(5.6);
-		std::vector<double> res = bst.contents_InOrder();
-		printvec(res);
-		std::cout << "\n\n";
+        BST::BinarySearchTree<double> bst(2.5);
+        bst.add(1.9);
+        bst.add(5.6);
+        bst.add(8.3);
+        bst.add(1.2);
+        bst.add(12.9);
+        bst.remove(5.6);
+        std::vector<double> res = bst.contents_InOrder();
+        printvec(res);
+        std::cout << "\n\n";
 
 
-		Tree::Tree<char> tree('A', true);
-		tree.add_child('C');
-		tree.add_child('B');
-		tree.add_child('G');
-		tree.add_child('M');
-		tree.goto_child(tree.find_child('G'));
-		tree.add_child('K');
-		tree.add_child('P');
-		tree.goto_root();
-		tree.goto_child(tree.find_child('B'));
-		tree.add_child('L');
-		tree.add_child('Z');
-		tree.goto_root();
-		tree.goto_child(tree.find_child('M'));
-		tree.add_child('X');
-		tree.add_child('Q');
-		tree.goto_child(tree.find_child('X'));
-		tree.add_child('F');
-		tree.add_child('O');
-		std::cout << "Max tree height: " << tree.max_height() << std::endl;
-		std::vector<char> t_res = tree.contents_InOrder();
-		printvec(t_res);
-		std::cout << "\n\n";
+        Tree::Tree<char> tree('A', true);
+        tree.add_child('C');
+        tree.add_child('B');
+        tree.add_child('G');
+        tree.add_child('M');
+        tree.goto_child(tree.find_child('G'));
+        tree.add_child('K');
+        tree.add_child('P');
+        tree.goto_root();
+        tree.goto_child(tree.find_child('B'));
+        tree.add_child('L');
+        tree.add_child('Z');
+        tree.goto_root();
+        tree.goto_child(tree.find_child('M'));
+        tree.add_child('X');
+        tree.add_child('Q');
+        tree.goto_child(tree.find_child('X'));
+        tree.add_child('F');
+        tree.add_child('O');
+        std::cout << "Max tree height: " << tree.max_height() << std::endl;
+        std::vector<char> t_res = tree.contents_InOrder();
+        printvec(t_res);
+        std::cout << "\n\n";
 
-		Vector::Vector<std::vector<int>> vector(3);
-		vector.push_back({ 2,3,4 });
-		vector.push_back({ 4 });
-		vector.push_back({6, 9});
-		for (size_t i = 0; i < vector.size(); ++i) {
-			printvec(vector[i]);
-		}
-		//Vector::Vector<int> vector;
-		//vector.push_back(2);
-		//vector.push_back(4);
-		//vector.push_back(8);
-		//vector.push_back(9);
-		//for (int i = 0; i < vector.size(); ++i)
-		//	std::cout << vector[i] << "\t";
-		std::cout << "\n\n";
+        Vector::Vector<std::vector<int>> vector(3);
+        vector.push_back({2, 3, 4});
+        vector.push_back({4});
+        vector.push_back({6, 9});
+        for (size_t i = 0; i < vector.size(); ++i) {
+            printvec(vector[i]);
+        }
+        //Vector::Vector<int> vector;
+        //vector.push_back(2);
+        //vector.push_back(4);
+        //vector.push_back(8);
+        //vector.push_back(9);
+        //for (int i = 0; i < vector.size(); ++i)
+        //	std::cout << vector[i] << "\t";
+        std::cout << "\n\n";
 
-	}
+    }
 
-	catch (std::invalid_argument& e) {
-		std::cerr << e.what() << std::endl;
-		return -1;
-	}
+    catch (std::invalid_argument& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
 
-	catch (std::runtime_error& e) {
-		std::cerr << e.what() << std::endl;
-		return -1;
-	}
+    catch (std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
 }
