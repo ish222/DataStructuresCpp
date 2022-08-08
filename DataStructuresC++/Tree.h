98ed97483ca54c7b3314973249335fd1d51d4d8e
ish222 <ishfaqwardag@hotmail.co.uk>
2022-08-08 13:44:50 +0100

    Improved tree implementation

--- DataStructuresC++/Tree.h
+++ DataStructuresC++/Tree.h
@@ -1,167 +1,192 @@
 #ifndef TREE_H
 #define TREE_H
 
-#include <vector>
+#include <algorithm>
 #include <stdexcept>
 #include <type_traits>
-#include <algorithm>
+#include <vector>
 
 namespace custom {
-    template<typename T>
-    class Tree {
-    public:
-        Tree(const T& data, const bool& ordered = false) {
-            Node* new_node = new Node(data);
-            root = new_node;
-            current_head = root;
-            this->ordered = ordered;  // Orders children in ascending values
-            if (ordered) {
-                static_assert(std::is_arithmetic<T>::value, "Ordered trees require arithmetic data objects");
-            }
-        }
+	template<typename T>
+	class Tree {
+	public:
+		Tree() noexcept : root(nullptr), current_head(nullptr), ordered(false) {}
 
-        void add_child(const T& data) {
-            if (current_head == nullptr) {
-                throw std::runtime_error("Current node is uninitialised, cannot add child.");
-                return;
-            }
-            Node* new_node = new Node(data);
-            if (ordered) {
-                int index = 0;
-                for (Node*& node : current_head->children) {
-                    if (data > node->data)
-                        ++index;
-                    else break;
-                }
-                current_head->children.insert(current_head->children.begin() + index, new_node);
-            } else current_head->children.push_back(new_node);
-        }
+		explicit Tree(const T& data, bool ordered = false) noexcept : ordered(ordered) {
+			root = new Node(data);
+			current_head = root;
+			if (ordered)
+				static_assert(std::is_arithmetic<T>::value, "Ordered trees require arithmetic data types");
+		}
 
-        std::vector<T> children_data() const {
-            if (current_head->children.empty()) {
-                throw std::runtime_error("Current node has no children.");
-                return {};
-            }
-            std::vector<T> ret = {};
-            for (const Node*& node : current_head->children) {
-                ret.push_back(node->data);
-            }
-            return ret;
-        }
+		explicit Tree(T&& data, bool ordered = false) noexcept : ordered(ordered) {
+			root = new Node(std::move(data));
+			current_head = root;
+			if (ordered)
+				static_assert(std::is_arithmetic<T>::value, "Ordered trees require arithmetic data types");
+		}
 
-        int find_child(const T& data) {
-            if (current_head->children.empty()) {
-                throw std::runtime_error("Current node has no children.");
-                return -1;
-            }
-            int index = 0;
-            for (Node* node : current_head->children) {
-                if (node->data == data)
-                    return index;
-                ++index;
-            }
-            return -1;
-        }
+		void add_child(const T& data) {
+			if (current_head) {
+				Node* new_node = new Node(data);
+				if (!current_head->children.empty() && ordered) {
+					for (size_t i = 0; i < current_head->children.size(); ++i) {
+						if (new_node->data < current_head->children[i]->data) {
+							current_head->children.insert(current_head->children.begin() + i, new_node);
+							return;
+						}
+					}
+				}
+				current_head->children.push_back(new_node);
+				return;
+			}
+			throw std::runtime_error("Current node is uninitialised, cannot add child.");
+		}
 
-        void goto_child(const int& index) {
-            if (current_head->children.size() - 1 < index || index < 0) {
-                throw std::invalid_argument("Index out of range.");
-                return;
-            }
-            current_head = current_head->children[index];
-        }
+		void add_child(T&& data) {
+			if (current_head) {
+				Node* new_node = new Node(std::move(data));
+				if (!current_head->children.empty() && ordered) {
+					for (size_t i = 0; i < current_head->children.size(); ++i) {
+						if (new_node->data < current_head->children[i]->data) {
+							current_head->children.insert(current_head->children.begin() + i, new_node);
+							return;
+						}
+					}
+				}
+				current_head->children.push_back(new_node);
+				return;
+			}
+			throw std::runtime_error("Current node is uninitialised, cannot add child.");
+		}
 
-        void goto_root() {
-            current_head = root;
-        }
+		[[nodiscard]] std::vector<T> children_data() const {
+			if (!current_head->children.empty()) {
+				std::vector<T> ret;
+				for (const Node*& node: current_head->children) {
+					ret.push_back(node->data);
+				}
+				return ret;
+			}
+			throw std::runtime_error("Current node has no children.");
+		}
 
-        int height() {
-            return get_depth(current_head);
-        }
+		[[nodiscard]] int find_child(const T& data) const {
+			if (!current_head->children.empty()) {
+				int index = 0;
+				for (Node* node: current_head->children) {
+					if (node->data == data)
+						return index;
+					++index;
+				}
+				return -1;
+			}
+			throw std::runtime_error("Current node has no children.");
+		}
 
-        int max_height() {
-            return get_depth(root);
-        }
+		void goto_child(const int& index) {
+			if (index < current_head->children.size() && index > -1)
+				current_head = current_head->children[index];
+			else
+				throw std::invalid_argument("Index out of range.");
+		}
 
-        operator bool() const {
-            return current_head != nullptr;
-        }
+		void goto_root() noexcept {
+			current_head = root;
+		}
 
-        std::vector<T> contents_InOrder() {
-            if (max_height() == 0) {
-                throw std::runtime_error("Error: Tree is empty, there is no content to return");
-                return std::vector<T>();
-            }
-            std::vector<T> temp = {};
-            return InOrder(root, temp);
-        }
+		[[nodiscard]] int current_height() const noexcept {
+			return get_depth(current_head);
+		}
 
-        void remove_child(const int& index) {
-            if (current_head->children.size() - 1 < index || index < 0) {
-                throw std::invalid_argument("Index out of range.");
-                return;
-            }
-            current_head->children.erase(current_head->children.begin() + index);
-        }
+		[[nodiscard]] int max_height() const noexcept {
+			return get_depth(root);
+		}
 
-        void delete_subtree() {
-            delete_tree(current_head);
-        }
+		[[nodiscard]] bool empty() const noexcept {
+			return root == nullptr;
+		}
 
-        void clear() {
-            delete_tree(root);
-            root = nullptr;
-            current_head = root;
-        }
+		explicit operator bool() const noexcept {
+			return current_head != nullptr;
+		}
 
-        ~Tree() {
-            if (root)
-                clear();
-        }
+		[[nodiscard]] std::vector<T> contents_InOrder() const {
+			if (root) {
+				std::vector<T> temp;
+				return InOrder(root, temp);
+			}
+			throw std::runtime_error("Error: Tree is empty, there is no content to return");
+		}
 
-    private:
-        struct Node {
-            T data;
-            std::vector<Node*> children;
+		void remove_child(const int& index) {
+			if (index < current_head->children.size() && index > -1)
+				current_head->children.erase(current_head->children.begin() + index);
+			else
+				throw std::invalid_argument("Index for remove_child is out of range");
+		}
 
-            Node(const T& data = 0) : data(data), children({}) {}
-        };
+		void delete_subtree() noexcept {
+			delete_tree(current_head);
+			current_head = nullptr;
+		}
 
-        Node* root;
-        Node* current_head;
-        bool ordered;
+		void clear() noexcept {
+			delete_tree(root);
+			root = nullptr;
+			current_head = root;
+		}
 
-        std::vector<T>& InOrder(Node* node, std::vector<T>& data) {
-            // TODO: FIX this function
-            if (!node) return data;
-            data.push_back(node->data);
-            for (Node*& child : node->children) {
-                data.push_back(child->data);
-                InOrder(child, data);
-            }
-            return data;
-        }
+		~Tree() {
+			if (root)
+				clear();
+		}
 
-        int get_depth(Node* node) {
-            if (!node) return 0;
-            int max_depth = 0;
-            for (Node* child : node->children) {
-                max_depth = std::max(max_depth, get_depth(child));
-            }
-            return ++max_depth;
-        }
+	private:
+		struct Node {
+			T data;
+			std::vector<Node*> children;
 
-        void delete_tree(Node*& node) {
-            if (!node) return;
-            if (node->children.empty()) {
-                delete node;
-                return;
-            }
-            for (Node*& child : node->children) {
-                delete_tree(child);
-            }
-        }
-    };
-}
+			explicit Node(const T& data) : data(data), children({}) {}
 
-#endif // TREE_H
+			explicit Node(T&& data) : data(std::move(data)), children({}) {}
+		};
+
+		Node* root;
+		Node* current_head;
+		bool ordered;// Orders children in ascending values
+
+		std::vector<T>& InOrder(Node* node, std::vector<T>& data) const {
+			if (!node) return data;
+			int child_count = node->children.size();
+			for (int i = 0; i < child_count - 1; ++i)
+				InOrder(node->children[i], data);
+			data.push_back(node->data);
+			if (child_count)
+				InOrder(node->children[child_count - 1], data);
+			return data;
+		}
+
+		int get_depth(Node* node) const noexcept {
+			if (!node) return 0;
+			int max_depth = 0;
+			for (Node* child: node->children) {
+				max_depth = std::max(max_depth, get_depth(child));
+			}
+			return ++max_depth;
+		}
+
+		void delete_tree(Node*& node) noexcept {
+			if (!node) return;
+			if (node->children.empty()) {
+				delete node;
+				return;
+			}
+			for (Node*& child: node->children) {
+				delete_tree(child);
+			}
+		}
+	};
+}// namespace custom
+
+#endif// TREE_H
